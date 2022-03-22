a0e6ff0fb63c4a1ae5176101aed3bbc3c4d0c421
DimitrisRammos <dimitris.p.rammos@gmail.com>
2022-03-22 20:44:16 +0200

    add

---
+++ JS/j.js
@@ -0,0 +1,14 @@
+"use strict";
+
+function star(){
+    location.href = "https://el.wikipedia.org/wiki/%CE%A0%CE%B1%CE%BD%CE%B1%CE%B8%CE%B7%CE%BD%CE%B1%CF%8A%CE%BA%CF%8C%CF%82_(%CE%BA%CE%B1%CE%BB%CE%B1%CE%B8%CE%BF%CF%83%CF%86%CE%B1%CE%AF%CF%81%CE%B9%CF%83%CE%B7_%CE%B1%CE%BD%CE%B4%CF%81%CF%8E%CE%BD)"
+}
+
+
+
+document.querySelector('#b1').addEventListener('click',star);
+document.querySelector('#b2').addEventListener('click',star);
+document.querySelector('#b3').addEventListener('click',star);
+document.querySelector('#b4').addEventListener('click',star);
+document.querySelector('#b5').addEventListener('click',star);
+document.querySelector('#b6').addEventListener('click',star);
